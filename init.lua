b75d45e6e7794ba274065ac368b49770f6082d4a
rekodev <reko.jsx@gmail.com>
2025-01-06 18:59:10 +0200

    install neominimap, adjust a couple of other settings

--- init.lua
+++ init.lua
@@ -23,6 +23,7 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
+vim.opt.signcolumn = "yes"
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.relativenumber = true
@@ -53,6 +54,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- tree sitter highlighting has priority over semantic tokens
 vim.highlight.priorities.semantic_tokens = 95
 vim.diagnostic.config({ update_in_insert = true })
+vim.keymap.set("n", "<leader>m", ":Neominimap toggle<CR>")
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -137,6 +139,11 @@ require("lazy").setup({
 		config = function()
 			local actions = require("telescope.actions")
 			require("telescope").setup({
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
 				defaults = {
 					mappings = {
 						i = {
@@ -183,7 +190,6 @@ require("lazy").setup({
 				filesystem = {
 					filtered_items = { hide_dotfiles = false, hide_gitignored = false, visible = true },
 					follow_current_file = { enabled = true, leave_dirs_open = false },
-					use_libuv_file_watcher = true,
 				},
 			})
 			vim.keymap.set("n", "<leader>b", ":Neotree toggle<CR>")
@@ -242,5 +248,53 @@ require("lazy").setup({
 		"nobbmaestro/nvim-andromeda",
 		dependencies = { "tjdevries/colorbuddy.nvim" },
 		opts = {},
+	},
+	{
+		"Isrothy/neominimap.nvim",
+		version = "v3.*.*",
+		enabled = true,
+		lazy = false, -- NOTE: NO NEED to Lazy load
+		-- Optional
+		keys = {
+			-- Global Minimap Controls
+			{ "<leader>nm", "<cmd>Neominimap toggle<cr>", desc = "Toggle global minimap" },
+			{ "<leader>no", "<cmd>Neominimap on<cr>", desc = "Enable global minimap" },
+			{ "<leader>nc", "<cmd>Neominimap off<cr>", desc = "Disable global minimap" },
+			{ "<leader>nr", "<cmd>Neominimap refresh<cr>", desc = "Refresh global minimap" },
+
+			-- Window-Specific Minimap Controls
+			{ "<leader>nwt", "<cmd>Neominimap winToggle<cr>", desc = "Toggle minimap for current window" },
+			{ "<leader>nwr", "<cmd>Neominimap winRefresh<cr>", desc = "Refresh minimap for current window" },
+			{ "<leader>nwo", "<cmd>Neominimap winOn<cr>", desc = "Enable minimap for current window" },
+			{ "<leader>nwc", "<cmd>Neominimap winOff<cr>", desc = "Disable minimap for current window" },
+
+			-- Tab-Specific Minimap Controls
+			{ "<leader>ntt", "<cmd>Neominimap tabToggle<cr>", desc = "Toggle minimap for current tab" },
+			{ "<leader>ntr", "<cmd>Neominimap tabRefresh<cr>", desc = "Refresh minimap for current tab" },
+			{ "<leader>nto", "<cmd>Neominimap tabOn<cr>", desc = "Enable minimap for current tab" },
+			{ "<leader>ntc", "<cmd>Neominimap tabOff<cr>", desc = "Disable minimap for current tab" },
+
+			-- Buffer-Specific Minimap Controls
+			{ "<leader>nbt", "<cmd>Neominimap bufToggle<cr>", desc = "Toggle minimap for current buffer" },
+			{ "<leader>nbr", "<cmd>Neominimap bufRefresh<cr>", desc = "Refresh minimap for current buffer" },
+			{ "<leader>nbo", "<cmd>Neominimap bufOn<cr>", desc = "Enable minimap for current buffer" },
+			{ "<leader>nbc", "<cmd>Neominimap bufOff<cr>", desc = "Disable minimap for current buffer" },
+
+			---Focus Controls
+			{ "<leader>nf", "<cmd>Neominimap focus<cr>", desc = "Focus on minimap" },
+			{ "<leader>nu", "<cmd>Neominimap unfocus<cr>", desc = "Unfocus minimap" },
+			{ "<leader>ns", "<cmd>Neominimap toggleFocus<cr>", desc = "Switch focus on minimap" },
+		},
+		init = function()
+			-- The following options are recommended when layout == "float"
+			vim.opt.wrap = false
+			vim.opt.sidescrolloff = 36 -- Set a large value
+
+			--- Put your configuration here
+			---@type Neominimap.UserConfig
+			vim.g.neominimap = {
+				auto_enable = true,
+			}
+		end,
 	},
 })
